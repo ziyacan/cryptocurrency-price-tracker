3ebee20eeadbd650131574dc8909d832b2854639
ziyacan <ziyacanistanbullu@gmail.com>
2022-12-18 22:17:00 +0300

    cors fix

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   const [search, setSearch] = useState("");
 
   useEffect(() => {
+    axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
     axios
       .get(
         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
